4e5c0925cbcfdea2a8d0368b6bfed96388f320da
Jamie <92243661+Jaorow@users.noreply.github.com>
2022-11-04 10:15:28 +1300

    csv reader start

--- app/adapters/csvdatareader.py
+++ app/adapters/csvdatareader.py
@@ -1,1 +1,24 @@
 """a data reader"""
+from app.domainmodel.post import Post
+from app.domainmodel.user import User
+
+import os
+import csv
+import ast
+
+class TrackCSVReader:
+
+    def __init__(self, albums_csv_file: str, tracks_csv_file: str):
+        if type(albums_csv_file) is str:
+            self.__albums_csv_file = albums_csv_file
+        else:
+            raise TypeError('albums_csv_file should be a type of string')
+
+        if type(tracks_csv_file) is str:
+            self.__tracks_csv_file = tracks_csv_file
+        else:
+            raise TypeError('tracks_csv_file should be a type of string')
+
+        # List of unique tracks
+        self.__dataset_of_posts = []
+        self.__dataset_of_users = []
